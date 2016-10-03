0aa933bb9e95efaac7884f9dac1aaed319fb3869
ehoversten <ehoversten@gmail.com>
2016-10-03 11:47:09 -0400

    added stuff

---
+++ db/migrate/20161003154520_add_attachment_:image_to_projects.rb
@@ -0,0 +1,11 @@
+class AddAttachment:imageToProjects < ActiveRecord::Migration
+  def self.up
+    change_table :projects do |t|
+      t.attachment ::image
+    end
+  end
+
+  def self.down
+    remove_attachment :projects, ::image
+  end
+end
